523326922d00f6a4218ee8506bf2b0e5b5dfa315
Melissa Giaime <melissa.giaime@gmail.com>
2015-11-30 14:08:07 -0800

    Adding updated file

--- JS-files/blog.js
+++ JS-files/blog.js
@@ -1,2 +1,18 @@
 var blog = {};
 blog.articles = [];
+
+var Article = function(props) {
+  this.title = props.title;
+  this.category = props.category;
+  this.author = props.author;
+  this.authorURL = props.authorURL;
+  this.publishedOn = props.publishedOn;
+  this.body = props.body;
+};
+
+Article.prototype.toHTML = function (argument) {
+  return '<article>' +
+    '<h2>' + this.title + '</h2>' +
+    '</article>';
+  // body...
+};
